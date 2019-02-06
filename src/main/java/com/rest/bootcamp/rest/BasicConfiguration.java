d485870b3b9e59cae6d1c4d68efb5238951a17a2
hsefein <grentorious@gmail.com>
2019-02-06 06:09:21 +0000

    Add files via upload

--- src/main/java/com/rest/bootcamp/rest/BasicConfiguration.java
+++ src/main/java/com/rest/bootcamp/rest/BasicConfiguration.java
@@ -1,7 +1,9 @@
-package com.rest.bootcamp.rest;
+package com.connect.connect2;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
